2db8f8eb70ec9dca0d326a38b55a2c36e5a08c08
Ivan Glushenkov <glushman966@gmail.com>
2025-12-01 21:39:22 +0300

    add filter_by_currency in generators.py

---
+++ src/generators.py
@@ -0,0 +1,14 @@
+from typing import Iterator
+
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
+    """Функция, которая фильтрует транзакции по коду валюты."""
+    for item in transactions:
+        op_currency = item.get("operationAmount", {}).get("currency", {}).get("code")
+        if op_currency == currency:
+            yield item
+
+usd_transactions = filter_by_currency(transactions, "USD")
+for _ in range(2):
+    print(next(usd_transactions))
+
