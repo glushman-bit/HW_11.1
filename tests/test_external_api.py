0590615f479a844d739c3ea3630d494d566c1142
Ivan Glushenkov <glushman966@gmail.com>
2025-12-14 03:29:29 +0300

    add test_external_api.py

---
+++ tests/test_external_api.py
@@ -0,0 +1,44 @@
+from unittest.mock import Mock, patch
+from src.external_api import converter_currency
+import pytest
+
+
+
+@patch("requests.get")
+def test_converter_currency_right(mock_get):
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"result": 79.67}
+    mock_get.return_value = mock_response
+
+    result = converter_currency(1, "USD")
+
+    assert result == 79.67
+
+    mock_get.assert_called_once()
+
+
+def test_converter_currency_invalid_currency():
+    with pytest.raises(ValueError, match="Код валюты не поддерживается"):
+        converter_currency(100, "GBP")
+
+
+@patch("requests.get")
+def test_converter_currency_http_error(mock_get):
+    mock_response = Mock()
+    mock_response.status_code = 500
+    mock_get.return_value = mock_response
+
+    with pytest.raises(Exception, match="Ошибка запроса: 500"):
+        converter_currency(1, "USD")
+
+
+def test_converter_currency_invalid_transaction():
+    with pytest.raises(ValueError, match="Введена неверная сумма транзакции"):
+        converter_currency("1", "USD")
+
+
+
+def test_converter_currency_negative_transaction():
+    with pytest.raises(ValueError, match="Сумма транзакции не может быть отрицательной"):
+        converter_currency(-1, "USD")
