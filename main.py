5d515ccaf9531acecf010bdad7fca966d64ff6d4
Ivan Glushenkov <glushman966@gmail.com>
2025-12-17 23:00:19 +0300

    add logger in masks.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.widget import mask_account_card
 if __name__ == "__main__":
     print(mask_account_card("Visa Platinum 7000792289999999"))
     print ("-" * 30)
-    print(mask_account_card("Счет 73654108430135874305"))
+    print(mask_account_card("Счет 73654108430135874390"))
     print("-" * 30)
     print(get_date("2024-03-11T02:26:18.671407"))
     print("-" * 30)
--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,12 @@
 from src.decorators import log
+import logging
 
+logger = logging.getLogger('masks.log')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/masks.log', mode='w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 @log()
 def get_mask_card_number(number_card: str, mask_char: str = "*", group_size: int = 4) -> str:
@@ -10,8 +17,10 @@ def get_mask_card_number(number_card: str, mask_char: str = "*", group_size: int
     length = len(number_card)
     # Исключаем номера не верной длинны
     if length not in (10, 16, 19):
+        logger.error(f"Неверная длинна номера карты: {length} цифр")
         raise ValueError("Неверная длина номера карты")
     if length == 0:
+        logger.error("Ничего не введено")
         raise ValueError("Ничего не введено")
     # Маскировка номера длинной 10
     if length == 10:
@@ -23,6 +32,7 @@ def get_mask_card_number(number_card: str, mask_char: str = "*", group_size: int
         mask_number = number_card[:6] + mask_section + number_card[-4:]
     # Группировка номера
     group_number = " ".join([mask_number[i : i + group_size] for i in range(0, len(mask_number), group_size)])
+    logger.info("Успешный ввод номера карты.")
     return group_number
 
 
@@ -30,6 +40,7 @@ def get_mask_card_number(number_card: str, mask_char: str = "*", group_size: int
 def get_mask_account(number_account: str, mask_char: str = "*") -> str:
     """Функция, скрывающая номер банковского счета"""
     if not number_account.isdigit():
+        logger.error("Неверный номер карты")
         raise ValueError("Номер должен содержать только цифры")
 
     mask_part = mask_char * 2
@@ -37,6 +48,8 @@ def get_mask_account(number_account: str, mask_char: str = "*") -> str:
     mask_number = mask_part + number_account[-4:]
 
     if len(number_account) != 20:
+        logger.error(f"Неверная длинна номера счета: {len(number_account)} цифр")
         raise ValueError("Введен не верный номер")
 
+    logger.info("Успешный ввод номера счета.")
     return mask_number
